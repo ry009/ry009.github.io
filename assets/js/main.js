6b62cca6b30a7364bfc1ebdfdeac5c320f551e90
ry003 <psycolor003@yahoo.co.jp>
2019-08-12 15:55:37 +0900

    no message

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,11 +42,11 @@ $(function () {
 	$divMain.append($divLoading);
 	$divMain.append($divBg);
 	$('body').removeClass('bgNone').prepend($divMain);
-});
 
-$(window).on('load', function () {
-	$('.loading').delay(700).fadeOut(2500).queue(function () {
-		this.remove();
+	$(window).on('load', function () {
+		$('#wrap').fadeIn("slow");
+		$divMain.delay(700).fadeOut(2500, function () {
+			$divMain.remove();
+		});
 	});
-	$('#wrap').fadeIn("slow");
 });
